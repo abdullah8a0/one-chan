8a61a33be9c93259a552c6310257daa2e6a1abb8
Haihan Wu <12011419@mail.sustech.edu.cn>
2022-11-08 15:45:50 -0500

    need new block diagram

--- src/DNN.sv
+++ src/DNN.sv
@@ -11,13 +11,23 @@ module DNN #(
     input wire en,
 
     //input grid
-    input wire input_iv,
+    input wire input_grid_iv,
     input wire [DATA_WIDTH-1:0] row_input_id [COL_NUM-1:0],
 
     //output grid
     output logic output_ov,
     output logic [DATA_WIDTH-1:0] output_od
 );
+
+    //fsm
+    localparam IDLE = 3'd0;
+    localparam INPUT_GRID = 3'd1;
+    localparam CONV_1 = 3'd2; // 3*3 kernel
+    localparam CONV_2 = 3'd3; // 3*3 kernel
+    localparam CONV_3 = 3'd4; // 1*3 kernel
+    localparam CLASSIFY_1 = 3'd5; // 8*8 weight matrix, 1*8 bias
+    localparam CLASSIFY_2 = 3'd6; // 8*8 weight matrix, 1*8 bias
+    localparam OUTPUT = 3'd7;
 
 endmodule
 
--- src/TPU.sv
+++ src/TPU.sv
@@ -5,7 +5,19 @@ module TPU(
     input wire nrst, 
 
     // spi interface
+    
 );
+
+    // FSM
+    localparam IDLE = 3'd0;
+    localparam INPUT_GRID_MOVE = 3'd1;
+    localparam COMPUTE_GRID = 3'd2;
+    localparam COMPUTE_DNN  = 3'd3;
+    localparam STORE_OUTPUT = 3'd4;
+    localparam SEND_OUTPUT  = 3'd5;
+
+    // global register
+    logic [7:0] move_cnt; // load one move, increase; else decrease by 1
 
 endmodule
 
